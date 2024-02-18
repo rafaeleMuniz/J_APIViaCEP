66f483477b63bd96ef1cf53cf870a8c9db25e839
Rafaele Muniz <rafaeleschmoeller@gmail.com>
2024-02-18 18:54:44 -0300

    "Projeto API CEP busca"

---
+++ src/ConsultaCep.java
@@ -0,0 +1,29 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaCep {
+
+    public Endereco buscaEndereco(String cep) {
+        URI endereco = URI.create("https://viacep.com.br/ws/" + cep + "/json/");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(endereco)
+                .build();
+
+        try {
+            HttpResponse<String> response = HttpClient
+                    .newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Endereco.class); //dentro do try - execução apenas se conseguir localizar
+        } catch (Exception e) { //"Exception" pois não sei ainda qual é o erro
+            throw new RuntimeException("Não foi possivel obter o endereço pelo CEP informado!");
+        }
+
+
+        }
+    }
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        ConsultaCep consultaCep = new ConsultaCep();
+
+        System.out.println("Digite o CEP para consultar: ");
+        var cep = leitura.nextLine();
+
+        try {
+            Endereco novoEndereco = consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.geraJson(novoEndereco);
+        }catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação.");
+        }
+
+    }
+}
